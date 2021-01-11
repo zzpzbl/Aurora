56128fd8cc6b6e3825f244e99e7cd05b6d167102
linchangyoo <1980055813@qq.com>
2021-01-11 15:02:36 +0800

    Manual annotation page finish 50%

--- src/views/Mark.vue
+++ src/views/Mark.vue
@@ -1,282 +1,276 @@
 <template>
-  <div class="au-mark">
-    <div class="search-card">
-      <p class="search-title">通过时间查询</p>
-      <el-form :model="timeForm" label-width="70px" label-position="left">
-        <el-form-item label="起始时间">
-          <el-date-picker
-            type="date"
-            placeholder="选择日期"
-            v-model="timeForm.beginDate"
-          ></el-date-picker>
-        </el-form-item>
-        <el-form-item label="结束时间">
-          <el-date-picker
-            type="date"
-            placeholder="选择日期"
-            v-model="timeForm.endDate"
-          ></el-date-picker
-        ></el-form-item>
-        <el-form-item label="波段">
-          <el-checkbox-group v-model="timeForm.band" :min="0" :max="1">
-            <el-checkbox-button label="G">G</el-checkbox-button>
-            <el-checkbox-button label="V">V</el-checkbox-button>
-            <el-checkbox-button label="R">R</el-checkbox-button>
-          </el-checkbox-group></el-form-item
-        >
-        <el-form-item label="类型">
-          <el-select v-model="timeForm.type" placeholder="请选择">
-            <el-option
-              v-for="(type, index) in types"
-              :key="index"
-              :value="index"
-              :label="type"
-            ></el-option>
-          </el-select>
-        </el-form-item>
-        <el-form-item>
-          <el-button @click="searchByTime">查询</el-button></el-form-item
-        ></el-form
-      >
-    </div>
-    <div class="result mark-result">
-      <div class="image-container mark-image-container">
-        <div class="current-image-container">
-          <div id="left-button" class="control-button" @click="handleLeftClick">
-            <i class="el-icon-arrow-left"></i>
-          </div>
-          <div
-            id="right-button"
-            class="control-button"
-            @click="handleRightClick"
-          >
-            <i class="el-icon-arrow-right"></i>
-          </div>
-          <img
-            v-show="currentImage"
-            ref="currentImage"
-            alt="查询结果图片"
-            class="current-image"
-          />
+    <div class="au-mark">
+        <div class="search-card">
+            <p class="search-title">条件查询</p>
+            <el-form :model="searchForm" label-position="left" label-width="70px">
+                <el-form-item label="起始时间">
+                    <el-date-picker
+                            placeholder="选择日期"
+                            type="date"
+                            v-model="searchForm.beginDate"
+                    ></el-date-picker>
+                </el-form-item>
+                <el-form-item label="结束时间">
+                    <el-date-picker
+                            placeholder="选择日期"
+                            type="date"
+                            v-model="searchForm.endDate"
+                    ></el-date-picker>
+                </el-form-item>
+                <el-form-item label="波段">
+                    <el-radio-group v-model="searchForm.band">
+                        <el-radio-button label="G"></el-radio-button>
+                        <el-radio-button label="V"></el-radio-button>
+                        <el-radio-button label="R"></el-radio-button>
+                    </el-radio-group>
+                </el-form-item>
+                <el-form-item>
+                    <el-button @click="searchByTime">查询</el-button>
+                </el-form-item>
+            </el-form>
         </div>
-        <div class="thumbs-container" ref="thumbs">
-          <img
-            v-for="(thumb, index) in thumbs"
-            :key="thumb.name"
-            :src="`data:image/png;base64,${thumb.thumb.data}`"
-            class="thumb"
-            @click="selectNewImage($event, thumb.name, index)"
-          />
-        </div>
-      </div>
-      <div class="mark-form-card">
-        <el-form :model="markForm">
-          <el-form-item>
-            <el-radio
-              v-model="markForm.type"
-              v-for="(type, index) in types.slice(1)"
-              :key="index"
-              :label="String(index+1)"
-              class="type-radio"
-              >{{ type }}</el-radio
-            >
-          </el-form-item>
-          <el-form-item>
-            <el-button @click="submitMark">提交</el-button>
-          </el-form-item>
-        </el-form>
-      </div>
+        <div class="result mark-result">
+<!--            <div class="image-container mark-image-container">-->
+<!--                <div class="current-image-container">-->
+<!--                    <div class="control-button" id="left-button">-->
+<!--                        <i class="el-icon-arrow-left"></i>-->
+<!--                    </div>-->
+<!--                    <div class="control-button" id="right-button">-->
+<!--                        <i class="el-icon-arrow-right"></i>-->
+<!--                    </div>-->
+<!--                    <img-->
+<!--                            alt="查询结果图片"-->
+<!--                            class="current-image"-->
+<!--                            src="https://ss3.bdstatic.com/70cFv8Sh_Q1YnxGkpoWK1HF6hhy/it/u=1329314336,1703660449&fm=26&gp=0.jpg"-->
+<!--                    />-->
+<!--                </div>-->
+<!--                <div class="images-container"></div>-->
+<!--            </div>-->
+<!--            <div class="mark-form-card">-->
+<!--                <el-form :model="markForm">-->
+<!--                    <el-form-item>-->
+<!--                        <el-radio-->
+<!--                                :key="index"-->
+<!--                                :label="index"-->
+<!--                                class="type-radio"-->
+<!--                                v-for="(type, index) in types"-->
+<!--                                v-model="markForm.type"-->
+<!--                        >{{ type }}-->
+<!--                        </el-radio>-->
+<!--                    </el-form-item>-->
+<!--                    <el-form-item>-->
+<!--                        <el-button @click="submitMark">提交</el-button>-->
+<!--                    </el-form-item>-->
+<!--                </el-form>-->
+<!--            </div>-->
+                    <div class="mark-thumbs-container" ref="thumbs">
+                        <div  v-for="(thumb, index) in thumbs"
+                              :key="thumb.name"
+                              class="mark-thumb-container"
+                              >
+                            <img
+                                 :src="`data:image/png;base64,${thumb.thumb}`"
+                                 class="mark-thumb"
+                                 @click="selectNewImage($event, thumb.name, index)"/>
+                            <p>{{thumb.name}}</p>
+                        </div>
+                    </div>
+            </div>
     </div>
-  </div>
 </template>
 
 <script>
-import { getThumb, getImageByName } from "../api/search";
-import { getTypeByName, updateType } from "../api/meta";
+    import {getThumb} from "../api/search";
 
-export default {
-  name: "AuMark",
-  data() {
-    return {
-      timeForm: {
-        beginDate: new Date("2003-12-01"),
-        endDate: new Date("2003-12-31"),
-        band: [],
-        type: 0,
-      },
-      types: ["不限", "多重弧", "帷幔型冕状", "放射型冕状", "热点型极光"],
-      markForm: {
-        type: 0,
-      },
-      currentImage: null,
-      thumbs: [],
-      currentThumbIndex: 0,
+    export default {
+        name: "AuMark",
+        data() {
+            return {
+                searchForm: {
+                    beginDate: "",
+                    endDate: "",
+                    band: "G",
+                },
+                types: ["多重弧", "帷幔型冕状", "放射型冕状", "热点型极光"],
+                markForm: {
+                    type: 0,
+                },
+                thumbs:[],
+            };
+        },
+        created() {
+            this.initData({});
+        },
+        methods: {
+            searchByTime() {
+                const params = {};
+
+                const beginDate = this.searchForm.beginDate;
+                const endDate = this.searchForm.endDate;
+                console.log(beginDate);
+                console.log(endDate);
+
+                if (beginDate == null || endDate == null) {
+                    this.$message.error("请输入正确格式的起始时间和结束时间");
+                    return;
+                } else if (beginDate > endDate) {
+                    this.$message.error("起始时间应小于结束时间");
+                    return;
+                } else {
+
+                    // this.$set(this.searchForm,'beginDate',current_date);
+                    // this.$set(this.searchForm,"endDate",current_date);
+
+                    // this.searchForm.beginDate = `${beginDate.getFullYear()}${String(
+                    //   beginDate.getMonth() + 1
+                    // ).padStart(2, "0")}${String(beginDate.getDate()).padStart(
+                    //   2,
+                    //   "0"
+                    // )}000000`;
+                    // this.searchForm.endDate = `${endDate.getFullYear()}${String(
+                    //   endDate.getMonth() + 1
+                    // ).padStart(2, "0")}${String(endDate.getDate()).padStart(2)}235959`;
+                    params.startTime = this.dateToString(this.searchForm.beginDate, true);
+                    params.endTime = this.dateToString(this.searchForm.endDate, false);
+                }
+
+                params.band = this.searchForm.band[0];
+
+                console.log(params);
+
+                getThumb(params)
+                    .then((res) => {
+                        // console.log("ret: " + res.data);
+                        this.thumbs = res.data;
+                        // console.log(this.thumb[0]);
+                        // 还原 查询表单和当前大图
+                        this.currentImage = null;
+                        console.log("name:" + this.thumbs[0].name);
+                        if (this.thumbs.length) {
+                            setTimeout(
+                                () => this.selectNewImage(null, this.thumbs[0].name),
+                                300
+                            );
+                        }
+                    })
+                    .catch((err) => console.error(err));
+            },
+            submitMark() {
+                console.log(this.markForm);
+            },
+            async initData() {
+                const now = new Date();
+                this.$set(this.searchForm, 'beginDate', now);
+                this.$set(this.searchForm, "endDate", now);
+            },
+            dateToString(date, isStart) {
+                let result = `${date.getFullYear()}${String(date.getMonth() + 1).padStart(2, "0")}${String(date.getDate()).padStart(2, "0")}`;
+                if (isStart == true) {
+                    result += '000000';
+                } else {
+                    result += '235959';
+                }
+                console.log(result);
+                return result;
+            }
+        },
     };
-  },
-  methods: {
-    restoreTimeForm() {
-      this.timeForm.beginDate = new Date("2003-12-01");
-      this.timeForm.endDate = new Date("2003-12-31");
-      // this.timeForm.band = [];
-    },
-    searchByTime() {
-      const beginDate = new Date(this.timeForm.beginDate);
-      const endDate = new Date(this.timeForm.endDate);
-      if (beginDate > endDate) {
-        this.$message.error("起始时间应小于结束时间");
-        return;
-      }
-
-      this.currentKeogram = null;
-
-      this.timeForm.beginDate = `${beginDate.getFullYear()}${String(
-        beginDate.getMonth() + 1
-      ).padStart(2, "0")}${String(beginDate.getDate()).padStart(2, "0")}000000`;
-      this.timeForm.endDate = `${endDate.getFullYear()}${String(
-        endDate.getMonth() + 1
-      ).padStart(2, "0")}${String(endDate.getDate()).padStart(2, '0')}235959`;
-      console.log(this.timeForm);
-
-      const params = {
-        startTime: this.timeForm.beginDate,
-        endTime: this.timeForm.endDate,
-      };
-
-      if (this.timeForm.manualtype !== '0') {
-        params.manualtype = this.timeForm.manualtype;
-      }
-
-      if (this.timeForm.band[0]) {
-        params.band = this.timeForm.band[0];
-      }
-
-      // console.log("params", params);
-
-      getThumb(params)
-        .then((res) => {
-          this.thumbs = res.data.data;
-          // console.log(this.thumbs);
-          // 还原 查询表单和当前大图
-          this.restoreTimeForm();
-          this.currentImage = null;
-          if (this.thumbs.length) {
-            setTimeout(
-              () => this.selectNewImage(null, this.thumbs[0].name),
-              300
-            );
-          }
-        })
-        .catch((err) => console.error(err));
-    },
-    submitMark() {
-      const params = {
-        name: this.currentImage.name,
-        manualtype: this.markForm.type,
-      };
-      // console.log('这个格式不行？', params);
-      updateType(params)
-        .then(() => {
-          this.$message.success('标注成功');
-        })
-        .catch((err) => console.error(err));
-      // console.log(this.markForm);
-      // console.log(this.currentImage);
-    },
-    selectNewImage(e, name, index) {
-      // console.log(name, e);
-      if (e) {
-        this.currentThumbIndex = index;
-        // console.log(this.$refs.thumbs.children);
-        this.$refs.thumbs.children.forEach((element) => {
-          element.classList.remove("active");
-        });
-        e.target.classList.add("active");
-      } else {
-        this.$refs.thumbs.children[this.currentThumbIndex].classList.add(
-          "active"
-        );
-        // 未知错误
-        // console.log(this.$refs.thumbs.children);
-        // console.log(this.$refs.thumbs.children[0]);
-      }
-      getImageByName(name)
-        .then((res) => {
-          this.currentImage = res.data.data;
-          console.log(this.currentImage);
-          this.$refs.currentImage.src = `data:image/png;base64,${this.currentImage.rawpic.data}`;
-        })
-        .catch((err) => console.error(err));
-      getTypeByName(name)
-        .then((res) => {
-          console.log('获取type', res.data.data.manualtype, res.data.data.name);
-          this.markForm.type = res.data.data.manualtype;
-        })
-        .catch((err) => console.error(err));
-    },
-    handleLeftClick() {
-      if (this.currentThumbIndex > 0) {
-        this.$refs.thumbs.children[this.currentThumbIndex].classList.remove(
-          "active"
-        );
-        this.currentThumbIndex--;
-        this.selectNewImage(null, this.thumbs[this.currentThumbIndex].name);
-      }
-    },
-    handleRightClick() {
-      if (this.currentThumbIndex < this.thumbs.length - 1) {
-        this.$refs.thumbs.children[this.currentThumbIndex].classList.remove(
-          "active"
-        );
-        this.currentThumbIndex++;
-        this.selectNewImage(null, this.thumbs[this.currentThumbIndex].name);
-      }
-    },
-  },
-};
 </script>
 
 <style>
-.au-mark {
-  display: flex;
-  align-items: flex-start;
-  padding: 30px 20px;
-}
+    .au-mark {
+        display: flex;
+        padding: 30px 20px;
+        flex: 1;
+        align-items: flex-start;
+    }
 
-.mark-result {
-  display: flex;
-  align-items: flex-start;
-  min-height: 270px;
-}
+    .mark-result {
+        display: flex;
+        align-items: stretch;
+    }
 
-.search-card {
-  flex: 2 1 100px;
-  border: 1px solid #e6e6e6;
-  box-shadow: 1px 1px 5px rgba(0, 0, 0, 0.2);
-  padding: 20px 0 0 20px;
-  border-radius: 10px;
-}
+    .search-card {
+        flex: 2 1;
+        border: 1px solid #e6e6e6;
+        box-shadow: 1px 1px 5px rgba(0, 0, 0, 0.2);
+        padding: 20px;
+        border-radius: 10px;
+    }
 
-.search-title {
-  margin: 0 0 25px 0;
-  color: #839c9d;
-  font-size: 20px;
-}
+    .search-title {
+        margin: 0 0 25px 0;
+        color: #839c9d;
+        font-size: 20px;
+    }
 
-.mark-form-card {
-  flex: 3;
-  min-width: 100px;
-  height: 200px;
-  margin: 0 20px 0 0;
-  padding: 30px 0 0 20px;
-}
 
-.type-radio {
-  margin: 0 0 25px 0;
-}
+    .image-container {
+        flex: 7;
+        margin: 20px;
+        border: 1px solid #e6e6e6;
+        min-width: 400px;
+        max-width: 883px;
+        text-align: center;
+    }
 
-.el-form-item__content {
-  display: flex;
-  flex-direction: column;
-  align-items: flex-start;
-}
+    .current-image-container {
+        position: relative;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        padding: 20px;
+    }
+
+    .current-image-container img {
+        width: 80%;
+        border: 1px solid #e6e6e6;
+    }
+
+    .mark-thumbs-container {
+        border: 1px solid #e6e6e6;
+        width: 100%;
+        height: 500px;
+        padding: 5px;
+        overflow: auto;
+        display: grid;
+        grid-template-columns: repeat(auto-fill, minmax(250px,1fr));
+        grid-auto-rows: minmax(300px,auto);
+    }
+
+
+    .mark-thumb-container{
+        margin:20px;
+        border: 1px solid #e6e6e6;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+    }
+    .mark-thumb {
+        width: 200px;
+        object-fit: contain;
+        padding-top: 20px;
+    }
+
+    .mark-thumb.active {
+        box-shadow: 0 0 0 3px #aaa;
+    }
+    .mark-form-card {
+        flex: 3;
+        min-width: 100px;
+        height: 200px;
+        margin: 20px 20px 0 0;
+        padding: 30px 0 0 20px;
+    }
+
+    .type-radio {
+        margin: 0 0 25px 0;
+    }
+
+    .el-form-item__content {
+        display: flex;
+        flex-direction: column;
+        align-items: flex-start;
+    }
 </style>
